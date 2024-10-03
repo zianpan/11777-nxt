1a5302a84dc60a1a4b37539aac3c668fd9b34edc
Zian Pan <seanpan2001@outlook.com>
2024-10-02 21:06:30 -0400

    Add aokvqa to the repo

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,18 @@
-import numpy as np
+import torch
+import clip
+from PIL import Image
+
+device = "mps" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+image = preprocess(Image.open("002877-R1-012-4A.jpg")).unsqueeze(0).to(device)
+text = clip.tokenize(["a man", "a dog", "a cat"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
